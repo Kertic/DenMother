641ef7ad42d6f013fc1469cb3c4332a8a1741e91
Unknown <BKrodriguez@fullsail.edu>
2019-01-25 23:23:41 -0600

    Added managers and input
    Added game manager and added basic input. Game manager has access to core gamewide data points.

---
+++ DenMother/Assets/Scripts/Structure/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Structure
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int MaxNestHealth = 3;
+        public int NestHealth;
+        public int StorageHealth;
+        public float EggWarmth;
+        public float FoodLevel;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ DenMother/Assets/Scripts/Structure/PlayerMover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Structure
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (Input.GetButton("Up"))
+            {
+                transform.Translate(Vector3.up * movementSpeed);
+            }
+
+            if (Input.GetButton("Down"))
+            {
+                transform.Translate(Vector3.down * movementSpeed);
+            }
+
+            if (Input.GetButton("Left"))
+            {
+                transform.Translate(Vector3.left * movementSpeed);
+            }
+
+            if (Input.GetButton("Right"))
+            {
+                transform.Translate(Vector3.right * movementSpeed);
+            }
+        }
+    }
+}
