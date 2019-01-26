4c0c50072be4f76e5e278ede75e424434ce4ec4b
Iris Nguyen <irisnguyen289@gmail.com>
2019-01-26 17:00:52 -0600

    Incubating
    Increase egg warmth level through time

---
+++ DenMother/Assets/Scripts/Structure/Incubating.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Contextuals;
+//using Structure.GameManager;
+
+public class Incubating : MonoBehaviour {
+    private bool collide;
+    public ContextualParent myObject;
+
+    private float rate;
+    // Use this for initialization
+    void Start () {
+        myObject = GameObject.FindObjectOfType<ContextualParent>;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ContextualParent.AreTwoRectsColliding()
+        if (collide)
+        {
+            eggwarm += eggwarm * rate;
+        }
+
+    }
+}
