d8e8f32f1e31686b943a3764723157fd1adfd3bb
Unknown <BKrodriguez@fullsail.edu>
2019-01-26 13:13:15 -0600

    Added basic collision detection
    (not working atm)

---
+++ DenMother/Assets/Scripts/Contextuals/ContextualParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.AccessControl;
+using UnityEngine;
+
+namespace Contextuals
+{
+	public class ContextualParent : MonoBehaviour
+	{
+
+		[SerializeField] private RectTransform motherTransform;
+
+		[SerializeField] private RectTransform ContextualTransform;
+		// Use this for initialization
+		void Start () {
+		
+		}
+
+
+		private void FixedUpdate()
+		{
+		//If the transforms are intersecting	
+
+		}
+
+		public bool AreTwoRectsColliding(RectTransform rectA, RectTransform rectB)
+		{
+			Vector2 originA = rectA.anchoredPosition - new Vector2(rectA.rect.width * 0.5f, rectA.rect.height * 0.5f);
+			Vector2 originB = rectB.anchoredPosition - new Vector2(rectB.rect.width * 0.5f, rectB.rect.height * 0.5f);
+			
+		}
+	}
+}
--- DenMother/Assets/Scripts/Structure/GameManager.cs
+++ DenMother/Assets/Scripts/Structure/GameManager.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Security.AccessControl;
 using UnityEngine;
 
 namespace Structure
 {
     public class GameManager : MonoBehaviour
     {
+        //These represent how many segments you can get with the food storage area and the nest
         public int MaxNestHealth = 3;
-        public int NestHealth;
-        public int StorageHealth;
+
+        public int MaxStorageHealth = 3;
+
+        //These are the actual healths of those two items. Use the mutator(s) to modify them.
+        public int nestHealth { get; private set; }
+        public int storageHealth { get; private set; }
         public float EggWarmth;
         public float FoodLevel;
+
+        public GameObject Mother;
 
         // Use this for initialization
         void Start()
@@ -19,6 +27,33 @@ namespace Structure
         // Update is called once per frame
         void Update()
         {
+        }
+
+        public void SetNestHealth(int newNestHealth)
+        {
+            if (newNestHealth < 0)
+            {
+                Debug.Log("Attempted to set nest health to negative number: " + newNestHealth + " Instead set it to 0");
+            }
+
+            if (newNestHealth > MaxNestHealth)
+            {
+                Debug.Log("Attempted to set nest health to value:" + newNestHealth + " greater than maximum of " +
+                          MaxNestHealth + ". Instead set it to Max");
+            }
+        }
+        public void SetFoodStorage(int newFoodStorage)
+        {
+            if (newFoodStorage < 0)
+            {
+                Debug.Log("Attempted to set food storage to negative number: " + newFoodStorage + " Instead set it to 0");
+            }
+
+            if (newFoodStorage > MaxNestHealth)
+            {
+                Debug.Log("Attempted to set food storage to value:" + newFoodStorage + " greater than maximum of " +
+                          MaxStorageHealth + ". Instead set it to Max");
+            }
         }
     }
 }
