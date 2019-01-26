2d11c2bed7968568eef490e92ca9f012c36a0afd
Iris Nguyen <irisnguyen289@gmail.com>
2019-01-26 17:07:36 -0600

    update incubating

--- DenMother/Assets/Scripts/Structure/Incubating.cs
+++ DenMother/Assets/Scripts/Structure/Incubating.cs
@@ -1,26 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Structure;
 using UnityEngine;
-using Contextuals;
-//using Structure.GameManager;
 
-public class Incubating : MonoBehaviour {
-    private bool collide;
-    public ContextualParent myObject;
-
+public class Incubating : GameManager
+{
     private float rate;
     // Use this for initialization
-    void Start () {
-        myObject = GameObject.FindObjectOfType<ContextualParent>;
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update () {
-        ContextualParent.AreTwoRectsColliding()
-        if (collide)
-        {
-            eggwarm += eggwarm * rate;
-        }
-
+    void Update()
+    {
+        EggWarmth += EggWarmth * rate * Time.deltaTime;
     }
 }
