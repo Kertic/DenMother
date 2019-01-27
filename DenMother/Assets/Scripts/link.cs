2f1b1c2d2591c95e48e9a2280d358cf6df8d000c
Abdulla <bonasr@uni.edu>
2019-01-27 14:36:18 -0600

    change name of files

--- DenMother/Assets/Scripts/link.cs
+++ DenMother/Assets/Scripts/link.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class link2 : MonoBehaviour 
+public class link : MonoBehaviour
 {
-
     public void Changescene(string BrennanTestScene)
-    { 
+    {
         SceneManager.LoadScene(BrennanTestScene);
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("BrennanTestScene");
+
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("BrennanTestScene");
     }
 }
--- DenMother/Assets/Scripts/link2.cs
+++ DenMother/Assets/Scripts/link2.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class link : MonoBehaviour
+public class link2 : MonoBehaviour 
 {
-    public void Changescene(string BrennanTestScene)
-    {
-        SceneManager.LoadScene(BrennanTestScene);
-        if (Input.anyKeyDown)
-            SceneManager.LoadScene("BrennanTestScene");
 
-    }
-    private void Update()
-    {
-        if (Input.anyKeyDown)
-            SceneManager.LoadScene("BrennanTestScene");
+    public void Changescene(string BrennanTestScene)
+    { 
+        SceneManager.LoadScene(BrennanTestScene);
     }
 }
