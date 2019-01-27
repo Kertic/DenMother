61b8bcf3ac1ebcbcc237b1b28878f69735a38c0a
Brennan Rodriguez <BKrodriguez@fullsail.edu>
2019-01-27 15:13:34 -0600

    cosmetics
    Cant go OOB anymore and forage bar

--- DenMother/Assets/Scripts/Player/PlayerMover.cs
+++ DenMother/Assets/Scripts/Player/PlayerMover.cs
@@ -8,6 +8,7 @@ namespace Structure
     {
         [SerializeField] private float movementSpeed;
 
+        public RectTransform Background;
 
         public void SetMovementSpeed(float inMovementSpeed)
         {
@@ -17,39 +18,54 @@ namespace Structure
 
         private void FixedUpdate()
         {
-            if (movementSpeed > 0.0f)
+            // GetComponent<Animator>().SetBool("isRunning", true);
+            if (Input.GetButton("Up"))
             {
-                // GetComponent<Animator>().SetBool("isRunning", true);
-                if (Input.GetButton("Up"))
-                {
-                    transform.Translate(Vector3.up * movementSpeed);
+                transform.Translate(Vector3.up * movementSpeed);
+                if (movementSpeed > 0)
                     GetComponent<SkeletonAnimation>().AnimationName = "Walk";
-                }
-                else if (Input.GetButton("Down"))
-                {
-                    transform.Translate(Vector3.down * movementSpeed);
+            }
+            else if (Input.GetButton("Down"))
+            {
+                transform.Translate(Vector3.down * movementSpeed);
+                if (movementSpeed > 0)
                     GetComponent<SkeletonAnimation>().AnimationName = "Walk";
-                }
+            }
 
-                else if (Input.GetButton("Left"))
+            else if (Input.GetButton("Left"))
+            {
+                transform.Translate(Vector3.left * movementSpeed);
+                if (movementSpeed > 0)
                 {
-                    transform.Translate(Vector3.left * movementSpeed);
                     transform.localScale = new Vector3(-10.0f, 10.0f, 10.0f);
                     GetComponent<SkeletonAnimation>().AnimationName = "Walk";
                 }
+            }
 
-                else if (Input.GetButton("Right"))
+            else if (Input.GetButton("Right"))
+            {
+                transform.Translate(Vector3.right * movementSpeed);
+                if (movementSpeed > 0)
                 {
-                    transform.Translate(Vector3.right * movementSpeed);
                     transform.localScale = new Vector3(10.0f, 10.0f, 10.0f);
                     GetComponent<SkeletonAnimation>().AnimationName = "Walk";
                 }
-                else
-                {
-                    //    GetComponent<Animator>().SetBool("isRunning", false);
-                    GetComponent<Skeleton>().SetToSetupPose();
-                }
             }
+            else
+            {
+                //    GetComponent<Animator>().SetBool("isRunning", false);
+                GetComponent<SkeletonAnimation>().AnimationName = "Idle";
+            }
+
+
+            if (transform.position.x <= 0)
+                transform.Translate(new Vector3(0 - transform.position.x, 0, 0));
+            if (transform.position.x >= 543)
+                transform.Translate(new Vector3(543 - transform.position.x, 0, 0));
+            if (transform.position.y <= 0)
+                transform.Translate(new Vector3(0, 0 - transform.position.y, 0));
+            if (transform.position.y >= 543)
+                transform.Translate(new Vector3(0, 543 - transform.position.y, 0));
         }
     }
 }
--- DenMother/Assets/Scripts/Structure/ForageBar.cs
+++ DenMother/Assets/Scripts/Structure/ForageBar.cs
@@ -4,23 +4,25 @@ using UnityEngine.UI;
 
 namespace Structure
 {
-	public class ForageBar : MonoBehaviour
-	{
+    public class ForageBar : MonoBehaviour
+    {
+        public Image uiFill;
+        public Image bkFill;
 
-		public Image uiFill;
-		public Image bkFill;
-		public GameManager manager;	
-		// Use this for initialization
-		void Start () {
-		
-		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-			uiFill.fillAmount = manager.timeSpentForaging / manager.forageDuration;
-			uiFill.gameObject.SetActive(uiFill.fillAmount >= 0.0f);
-			bkFill.gameObject.SetActive(uiFill.fillAmount >= 0.0f);
-		}
-	}
+        public GameManager manager;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            uiFill.fillAmount = manager.timeSpentForaging / manager.forageDuration;
+            bool live = manager.timeSpentForaging / manager.forageDuration > 0.0f;
+            uiFill.gameObject.SetActive(uiFill.fillAmount >= 0.0f);
+            bkFill.gameObject.SetActive(live);
+        }
+    }
 }
