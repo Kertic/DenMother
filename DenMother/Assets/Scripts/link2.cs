e0d5d1b376eee30301812be7c7875de68b5a4872
Abdulla <bonasr@uni.edu>
2019-01-27 13:47:12 -0600

    tutorial screen

--- DenMother/Assets/Scripts/link2.cs
+++ DenMother/Assets/Scripts/link2.cs
@@ -8,5 +8,13 @@ public class link : MonoBehaviour
     public void Changescene(string BrennanTestScene)
     {
         SceneManager.LoadScene(BrennanTestScene);
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("BrennanTestScene");
+
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("BrennanTestScene");
     }
 }
