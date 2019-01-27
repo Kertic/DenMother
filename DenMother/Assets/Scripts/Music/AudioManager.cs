0b7e9fed213fd2b8b4e6ec6e4d7cc8925b86aaca
Iris Nguyen <irisnguyen289@gmail.com>
2019-01-26 23:37:14 -0600

    Update Audio Manager

--- DenMother/Assets/Scripts/Music/AudioManager.cs
+++ DenMother/Assets/Scripts/Music/AudioManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
 using System;
+using System.Collections;
 
-public class AudioManager : MonoBehaviour {
+public class AudioManager : MonoBehaviour
+{
 
     public sound[] Sound;
 
     public static AudioManager instance;
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
         if (instance == null)
         {
             instance = this;
@@ -21,7 +24,7 @@ public class AudioManager : MonoBehaviour {
         }
         DontDestroyOnLoad(gameObject);
 
-		foreach (sound s in Sound)
+        foreach (sound s in Sound)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
@@ -35,18 +38,65 @@ public class AudioManager : MonoBehaviour {
 
     public void Start()
     {
-        //Play( Theme song );
+        Play("Theme");
     }
 
-    public void Play(string name)
+    //public void Play(string name)
+    //{
+    //    sound s = Array.Find(Sound, sound => sound.name == name);
+    //    if (s == null)
+    //    {
+    //        Debug.LogWarning("Sound: "+name +"not found1!");
+    //        return;
+    //    }
+    //    s.source.Play();
+    //}
+
+    public void Stop(string name)
     {
         sound s = Array.Find(Sound, sound => sound.name == name);
         if (s == null)
         {
-            Debug.LogWarning("Sound: "+name +"not found1!");
+            Debug.LogWarning("Sound: " + name + "not found1!");
             return;
         }
-        s.source.Play();
+        s.source.Stop();
     }
 
+    public void Play(string name)
+    {
+        StopAllCoroutines();
+        if (Sound != null) StartCoroutine(EndSound());
+
+        sound s = Array.Find(Sound, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Music " + name + " not found.");
+            return;
+        }
+        StartCoroutine(StartSound());
+    }
+
+    private IEnumerator EndSound()
+    {
+        AudioSource oldSound = Sound.source;
+        while (oldSound.volume > 0)
+        {
+            oldSound.volume -= 0.01f;
+            yield return null;
+        }
+        oldSound.Stop();
+    }
+
+    private IEnumerator StartSound()
+    {
+        Sound.source.Play();
+        float volume = 0f;
+        do
+        {
+            Sound.source.volume = volume;
+            volume += 0.01f;
+            yield return null;
+        } while (Sound.source.volume <= Sound.volume);
+    }
 }
